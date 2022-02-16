f149169ab2e11a07cb5beb7abda9331cbe2213a6
r3d-scarf <pavel.shcherbatyi@protonmail.com>
2022-02-16 21:25:20 +0200

    Project initialization commit (2022.02.16)

---
+++ src/main/java/com/redscarf/dreamroutes/DreamroutesApplication.java
@@ -0,0 +1,13 @@
+package com.redscarf.dreamroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamroutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamroutesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redscarf/dreamroutes/DreamroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redscarf.dreamroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamroutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

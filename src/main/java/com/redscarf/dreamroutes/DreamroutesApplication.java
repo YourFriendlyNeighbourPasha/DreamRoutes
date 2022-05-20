3a5cd7dc7c95d148596f692956dd720ffb0a7748
r3d-scarf <pavel.shcherbatyi@protonmail.com>
2022-05-20 03:09:55 +0300

    Added Swagger support

--- src/main/java/com/redscarf/dreamroutes/DreamroutesApplication.java
+++ src/main/java/com/redscarf/dreamroutes/DreamroutesApplication.java
@@ -1,11 +1,9 @@
 package com.redscarf.dreamroutes;
 
-import com.redscarf.dreamroutes.utils.SpringSecurityAuditorAware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -22,11 +20,6 @@ public class DreamroutesApplication {
             flyway.repair();
             flyway.migrate();
         };
-    }
-
-    @Bean
-    public AuditorAware<String> auditorAware() {
-        return new SpringSecurityAuditorAware();
     }
 
     public static void main(String[] args) {
